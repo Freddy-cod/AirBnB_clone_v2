cb0ab296706c39ef7725b63657ad2e481af339e6
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-08-11 11:15:37 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,99 +1,52 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+
 import unittest
-import datetime
-from uuid import UUID
-import json
 import os
 
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
-class test_basemodel(unittest.TestCase):
-    """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
+class TestAmenity(unittest.TestCase):
 
-    def setUp(self):
-        """ """
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls.amenity1 = Amenity()
+        cls.amenity1.name = "Hot Tub"
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
+        del cls.amenity1
         try:
-            os.remove('file.json')
-        except:
+            os.remove("file.json")
+        except FileNotFoundError:
             pass
 
-    def test_default(self):
-        """ """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.amenity1.__class__, BaseModel), True)
 
-    def test_kwargs(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        new = BaseModel(**copy)
-        self.assertFalse(new is i)
+    def test_checking_for_functions(self):
+        self.assertIsNotNone(Amenity.__doc__)
 
-    def test_kwargs_int(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        copy.update({1: 2})
-        with self.assertRaises(TypeError):
-            new = BaseModel(**copy)
+    def test_has_attributes(self):
+        self.assertTrue('id' in self.amenity1.__dict__)
+        self.assertTrue('created_at' in self.amenity1.__dict__)
+        self.assertTrue('updated_at' in self.amenity1.__dict__)
+        self.assertTrue('name' in self.amenity1.__dict__)
 
+    def test_attributes_are_strings(self):
+        self.assertEqual(type(self.amenity1.name), str)
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
     def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
-        with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
+        self.amenity1.save()
+        self.assertNotEqual(self.amenity1.created_at, self.amenity1.updated_at)
 
-    def test_str(self):
-        """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+    def test_to_dict(self):
+        self.assertEqual('to_dict' in dir(self.amenity1), True)
 
-    def test_todict(self):
-        """ """
-        i = self.value()
-        n = i.to_dict()
-        self.assertEqual(i.to_dict(), n)
 
-    def test_kwargs_none(self):
-        """ """
-        n = {None: None}
-        with self.assertRaises(TypeError):
-            new = self.value(**n)
-
-    def test_kwargs_one(self):
-        """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
-
-    def test_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.id), str)
-
-    def test_created_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
-
-    def test_updated_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+if __name__ == "__main__":
+    unittest.main()
